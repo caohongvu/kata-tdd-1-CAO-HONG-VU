2c0eec15d1032284c0b9a18ba0d39e3f59fdd633
Cao Hong Vu <caohongvu@gmail.com>
2016-06-28 21:23:46 +0700

    add grunt-contrib-watch task to always monitor js file changing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,21 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		jshint: {
+		jshint: { 
 			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
 			options: {
 				globals: {
 					jQuery: true
 				}
 			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
 	grunt.registerTask('default', ['jshint']);
 };
