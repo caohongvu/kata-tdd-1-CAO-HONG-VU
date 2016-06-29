984e3b6c7541f5900e058b2c378859c43c0d083f
Cao Hong Vu <caohongvu@gmail.com>
2016-06-30 01:18:10 +0700

    implement to pass test case 2: "If only one number is inputted, it will return that number"

--- src/controllers/stringCalculatorCtrl.js
+++ src/controllers/stringCalculatorCtrl.js
@@ -5,6 +5,8 @@ stringCalculator.controller('stringCalculatorCtrl', function StringCalculatorCtr
 	$scope.add = function () {
 		if($scope.inputString === ""){
 			$scope.result = 0;
+		} else if($scope.inputString.indexOf(",") < 0) {
+			$scope.result = $scope.inputString;
 		}
 	};
 });
