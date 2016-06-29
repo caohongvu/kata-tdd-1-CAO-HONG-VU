c4cbe307661f1dcf4e79e9bff0858246381cceb4
Cao Hong Vu <caohongvu@gmail.com>
2016-06-30 01:29:39 +0700

    Implements to pass test case 3: "If have two number are inputted, it will return their sum"

--- src/controllers/stringCalculatorCtrl.js
+++ src/controllers/stringCalculatorCtrl.js
@@ -5,8 +5,11 @@ stringCalculator.controller('stringCalculatorCtrl', function StringCalculatorCtr
 	$scope.add = function () {
 		if($scope.inputString === ""){
 			$scope.result = 0;
-		} else if($scope.inputString.indexOf(",") < 0) {
+		} else if ($scope.inputString.indexOf(",") < 0) {
 			$scope.result = $scope.inputString;
+		} else if ($scope.inputString.indexOf(",") > 0) {
+			var inputtedNumbers = $scope.inputString.split(',');
+			$scope.result = Number(inputtedNumbers[0]) + Number(inputtedNumbers[1]);
 		}
 	};
 });
